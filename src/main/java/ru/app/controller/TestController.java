6836eeb78e7ef65eb05da2a99d152d63691f5e6a
k.velichko <k.velichko@msn.com>
2021-03-14 03:20:22 +0300

    test logs

--- src/main/java/ru/app/controller/TestController.java
+++ src/main/java/ru/app/controller/TestController.java
@@ -1,16 +1,20 @@
 package ru.app.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 
 @RestController
+@Slf4j
 public class TestController {
     private static final Random random = new Random();
 
     @GetMapping("")
     public String test() {
-        return "Test message " + random.nextInt(100);
+        String message = "Test message " + random.nextInt(100);
+        log.info(message);
+        return message;
     }
 }
